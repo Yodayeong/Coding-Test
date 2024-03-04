efc0886add4bddf45581c0cdefe4a38099ba1362
yodayeong <duekdud01@naver.com>
2024-03-05 00:56:36 +0900

    feat: cpp 입출력 update

--- chapter2/2-12.cpp
+++ chapter2/2-12.cpp
@@ -9,6 +9,6 @@ double b = 1.223123;
 int main() {
     printf("아이엠어 아이언맨 : %d\n", a);
     printf("아이엠어 아이언맨 : %c\n", s);
-    printf("아이엠어 아이언맨 : %s\n", str.c_str());
+    printf("아이엠어 아이언맨 : %s\n", str.c_str()); //string은 포인터 타입으로 바꿔줘야 하기 때문!
     printf("아이엠어 아이언맨 : %lf\n", b);
 }
